636161620c77ea39553be6b24613c2a0348c762a
1Maxuta1 <maksklas_1@ukr.net>
2023-03-10 23:29:48 +0200

    Navigation
    Ready Navigation component in Header

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Dropdown from "./buttons/Dropdown";
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <div className="navigation__wrapper flex justify-between items-center bg-[#ebe0bf] flex-row h-28">
+        <div className="navigation__items ">
+          <ul className="flex flex-row gap-4">
+            <li className="navigation__item">
+              <a>About us</a>
+            </li>
+            <li className="navigation__item">
+              <Dropdown />
+            </li>
+            <li className="navigation__item">
+              <a>Reviews</a>
+            </li>
+            <li className="navigation__item">
+              <a>Contact us</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navigation__logo w-40">
+          <a href="#">
+            <img src="/img/logo.png" alt="Remsvol" />
+          </a>
+        </div>
+
+        <div className="navigation__icons flex gap-4 items-center">
+          <a href="#">
+            <img className="h-6" src="/img/icons/mail-icon.png" alt="mailto" />
+          </a>
+          <a href="#">
+            <img
+              className="h-6"
+              src="/img/icons/instagram-icon.png"
+              alt="instagram"
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-6"
+              src="/img/icons/facebook-icon.png"
+              alt="mailto"
+            />
+          </a>
+          <a href="#">
+            <img
+              className="h-6"
+              src="/img/icons/youtube-icon.png"
+              alt="mailto"
+            />
+          </a>
+          <a href="#">
+            <img className="h-6" src="/img/icons/phone-icon.png" alt="mailto" />
+          </a>
+
+          <div className="navigation__button bg-[#c09559] p-3 rounded-lg hover:bg-opacity-70 duration-300">
+            <a>Application</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
