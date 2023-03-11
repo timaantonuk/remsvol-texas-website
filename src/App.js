8f955d023272ce29193f27841134558f5f0ebb96
1Maxuta1 <maksklas_1@ukr.net>
2023-03-11 20:35:27 +0200

    About Us component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AboutUs from "./components/AboutUs";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AboutUs />
       {/* <div className='Container border-green-500 border-2 max-w-6xl mx-auto h-full'></div> */}
     </div>
   );
