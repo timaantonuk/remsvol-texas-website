61fd09be6bd00f3757d05f4bbfb1c2f93562f88d
1Maxuta1 <maksklas_1@ukr.net>
2023-03-12 15:11:28 +0200

    Services section created, Sticky header
    Services section created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import AboutUs from "./components/AboutUs";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import Services from "./components/Services";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AboutUs />
+      <Services />
       {/* <div className='Container border-green-500 border-2 max-w-6xl mx-auto h-full'></div> */}
     </div>
   );
